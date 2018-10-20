00bf699cbcf11dcffbf9740e07abd3ea240438ff
Rafael Guedes <rafaelmenta@gmail.com>
2018-10-19 18:26:16 -0700

    Sync stats when calculating performance

--- graphql/mutation/rule/team.performance.js
+++ graphql/mutation/rule/team.performance.js
@@ -101,13 +101,51 @@ export const TeamPerformanceMutation = {
                       })
                       .then(playerUpdates => Conn.query(playerUpdates.join(' '), {transaction: t}))
                       .then(playerUpdates => TeamPerformance.update({
-                      minutes_pg: minutes.PG,
-                      minutes_sg: minutes.SG,
-                      minutes_sf: minutes.SF,
-                      minutes_pf: minutes.PF,
-                      minutes_c: minutes.C,
-                      fantasy_points: score.toFixed(3),
-                    }, { where: { tpf_code: team.tpf_code}, transaction: t }));
+                        minutes_pg: minutes.PG,
+                        minutes_sg: minutes.SG,
+                        minutes_sf: minutes.SF,
+                        minutes_pf: minutes.PF,
+                        minutes_c: minutes.C,
+                        fantasy_points: score.toFixed(3),
+                        }, { where: { tpf_code: team.tpf_code}, transaction: t }))
+                      .then(() => {
+                        const playerStats = `
+                          UPDATE player_stats ps
+                          JOIN player_stats_view v ON ps.id_player = v.id_player and ps.id_season = v.id_season
+                          JOIN season s ON s.id_season=ps.id_season
+                          SET
+                            ps.games = v.games,
+                            ps.minutes = v.minutes,
+                            ps.points = v.points,
+                            ps.field_goal_attempts = v.field_goal_attempts,
+                            ps.free_throw_attempts = v.free_throw_attempts,
+                            ps.offensive_rebounds = v.offensive_rebounds,
+                            ps.defensive_rebounds = v.defensive_rebounds,
+                            ps.assists = v.assists,
+                            ps.turnovers = v.turnovers,
+                            ps.steals = v.steals,
+                            ps.blocks = v.blocks,
+                            ps.personal_fouls = v.personal_fouls,
+                            ps.fantasy_points_per_minutes = v.fantasy_points_per_minutes,
+                            ps.fantasy_points = v.fantasy_points
+                          WHERE s.current = true;`;
+                        return Conn.query(playerStats, {transaction: t})
+                      })
+                      .then(() => {
+                        const teamStats = `
+                          UPDATE team_stats ts
+                          JOIN team_stats_view v ON ts.id_sl = v.id_sl and ts.id_season=v.id_season
+                          JOIN season s ON s.id_season=ps.id_season
+                          SET
+                            ts.fantasy_points = v.fantasy_points,
+                            ts.minutes_pg = v.minutes_pg,
+                            ts.minutes_sg = v.minutes_sg,
+                            ts.minutes_sf = v.minutes_sf,
+                            ts.minutes_pf = v.minutes_pf,
+                            ts.minutes_c = v.minutes_c
+                          WHERE s.current = true;`;
+                        return Conn.query(teamStats, {transaction: t})
+                      });
                   });
                   return Promise.all(teamsUpdates);
                  });
